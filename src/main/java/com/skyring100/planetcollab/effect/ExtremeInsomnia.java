14ed8002b8c418bd8fc8e361e0be60c6bcb35701
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2023-12-14 20:08:52 -0800

    Extreme insomnia no longer instant kills and now damages slightly with a disorienting effect

--- src/main/java/com/skyring100/planetcollab/effect/ExtremeInsomnia.java
+++ src/main/java/com/skyring100/planetcollab/effect/ExtremeInsomnia.java
@@ -3,6 +3,8 @@ package com.skyring100.planetcollab.effect;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 public class ExtremeInsomnia extends MobEffect {
 
@@ -12,7 +14,10 @@ public class ExtremeInsomnia extends MobEffect {
     @Override
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
         if(pLivingEntity.isSleeping()){
-            pLivingEntity.hurt(DamageSource.MAGIC, 100f);
+            int disorientTime = 200;
+            pLivingEntity.hurt(DamageSource.MAGIC, 5f);
+            pLivingEntity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS,disorientTime));
+            pLivingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, disorientTime));
         }
     }
     @Override
